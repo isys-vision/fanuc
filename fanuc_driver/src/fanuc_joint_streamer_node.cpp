72e401130f7e6cc74d1903faa7e9653669ad13da
Lionel Burg <lionel.burg@optonic.com>
2025-08-12 14:55:01 +0200

    use rosparam for watchdog_timeout

--- fanuc_driver/src/fanuc_joint_streamer_node.cpp
+++ fanuc_driver/src/fanuc_joint_streamer_node.cpp
@@ -113,7 +113,7 @@ public:
   void feed_status_watchdog() {
     ROS_INFO("Start feed_status_watchdog monitoring thread");
     while (ros::ok()) {
-      if (ros::Time::now() - this->streaming_start_ < ros::Duration(FEED_TIMEOUT_PERIOD_)) {
+      if (ros::Time::now() - this->streaming_start_ < ros::Duration(fanuc_move_action_timeout_s)) {
         // only if this action has not timed out
 
         control_msgs::FollowJointTrajectoryFeedback control_state;
